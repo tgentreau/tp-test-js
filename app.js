1f22503fd9be9adb2884fe0b79a6f13b85ea8d73
tgentreau <gentreau.thomas@gmail.com>
2022-04-28 11:59:35 +0200

    first

---
+++ app.js
@@ -0,0 +1,47 @@
+const produit = {
+    nom: "Test",
+    prix: 15,
+    poids: 0.5,
+    quantite: 2
+}
+
+const produit2 = {
+    nom: "Test2",
+    prix: 20,
+    poids: 0.5,
+    quantite: 1
+}
+
+const addProduit = (produits) => {
+    let cart = {
+        produits
+    }
+    return cart
+}
+
+const createPanier = (produit) => {
+    const cart = addProduit(produit)
+    return cart
+}
+
+// console.log(createPanier([produit,produit2]))
+
+const getPrixTotalPanier = () => {
+    const panier = createPanier([produit,produit2])
+    let prix = 0
+    for (let i = 0; i < panier.produits.length; i++) {
+        prix += panier.produits[i].prix * panier.produits[i].quantite    
+    }
+    return prix
+}
+
+// console.log(getPrixTotalPanier());
+
+const prixTVA = (tva) => {
+    const prix = getPrixTotalPanier()
+    const prixTva = (prix * tva) / 100
+    const prixFinal = prix + prixTva
+    return prixFinal
+}
+
+console.log(prixTVA(20));
