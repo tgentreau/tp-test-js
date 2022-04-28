67a0016cdbc7668eca2e630930be327e9e5288fa
tgentreau <gentreau.thomas@gmail.com>
2022-04-28 14:54:03 +0200

    modif

--- test/demo.test.js
+++ test/demo.test.js
@@ -4,6 +4,11 @@ describe("TEST prix total du panier", () => {
     it("générer un prix total", () => {
         debugger
         getPrixTotalPanier().should.be.a.Number()
+    })
+})
+describe("TEST tva number", () => {
+    it("générer une tva", () => {
+        debugger
         prixTVA().should.be.a.Number()
     })
 })
